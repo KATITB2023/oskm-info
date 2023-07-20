e7973941693bf72c5f1d42363a0adec38d313684
msyahrulsp <mssp892@gmail.com>
2023-07-20 14:57:16 +0700

    feat: add landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,90 @@
+import { Box, Center, Heading, Image } from '@chakra-ui/react';
 import { type NextPage } from 'next';
-import Layout from '~/layout';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
-  return <Layout title='Home'>Ini Home</Layout>;
+  return (
+    <>
+      <Head>
+        <title>OSKM ITB 2023</title>
+        <meta
+          name='description'
+          content='Sebuah website yang akan digunakan untuk acara OSKM ITB 2023'
+        />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+      <Center minH='100vh' position='relative'>
+        <Image
+          src='/images/background.png'
+          alt=''
+          position='absolute'
+          zIndex='-1'
+          w='100%'
+          h='100%'
+          objectFit='cover'
+          objectPosition='center'
+          draggable='false'
+        />
+        <Image
+          src='/images/shooting-star-left.png'
+          display={{ base: 'none', md: 'block' }}
+          alt=''
+          position='absolute'
+          w='35%'
+          top='0'
+          left='0'
+          zIndex='10'
+          draggable='false'
+        />
+        <Image
+          src='/images/shooting-star-right.png'
+          display={{ base: 'none', md: 'block' }}
+          alt=''
+          position='absolute'
+          w='35%'
+          top='0'
+          right='0'
+          zIndex='10'
+          draggable='false'
+        />
+        <Image
+          src='/images/mini-star-right.png'
+          display={{ base: 'none', md: 'block' }}
+          alt=''
+          position='absolute'
+          w='35%'
+          bottom='0'
+          right='0'
+          zIndex='10'
+          draggable='false'
+        />
+        <Image
+          src='/images/mini-star-left.png'
+          display={{ base: 'none', md: 'block' }}
+          alt=''
+          position='absolute'
+          w='35%'
+          bottom='0'
+          left='0'
+          zIndex='10'
+          draggable='false'
+        />
+        <Heading
+          fontSize={{ base: '2xl', lg: '6xl' }}
+          textAlign='center'
+          px={4}
+          textShadow={{ base: '0px 1px #E77A23', lg: '0px 3px #E77A23' }}
+        >
+          <Box as='span' color='#8D47E5'>
+            PREPARE TO {` `}
+          </Box>
+          <Box as='span' color='#4A58F6'>
+            launch
+          </Box>
+        </Heading>
+      </Center>
+    </>
+  );
 };
 
 export default Home;
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,27 +1,27 @@
-import { extendTheme } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   fonts: {
-    heading: "Bodwars",
-    body: "SomarRounded-Regular"
+    heading: 'Bodwars',
+    body: 'SomarRounded-Regular'
   },
   styles: {
     global: {
       body: {
-        bg: "#12122E",
-        color: "white"
+        bg: '#12122E',
+        color: 'white'
       },
-      "*": {
-        "&::-webkit-scrollbar": {
-          w: "2",
-          h: "1.5"
+      '*': {
+        '&::-webkit-scrollbar': {
+          w: '2',
+          h: '1.5'
         },
-        "&::-webkit-scrollbar-track": {
-          backgroundColor: "transparent"
+        '&::-webkit-scrollbar-track': {
+          backgroundColor: 'transparent'
         },
-        "&::-webkit-scrollbar-thumb": {
-          backgroundColor: "#1C939A",
-          borderRadius: "4"
+        '&::-webkit-scrollbar-thumb': {
+          backgroundColor: '#1C939A',
+          borderRadius: '4'
         }
       }
     }
