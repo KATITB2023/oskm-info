ede478431dfd9504f997d25213fbe35ac0928ed2
YakobusIP <yakobusiryantoprasethio@gmail.com>
2023-07-26 16:08:21 +0700

    feat: broken 2nd form

---
+++ src/components/form/showcase/SecondForm.tsx
@@ -0,0 +1,198 @@
+import {
+  Box,
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  VStack,
+  Flex,
+  useToast,
+  Select
+} from '@chakra-ui/react';
+import { TRPCClientError } from '@trpc/client';
+import {
+  type BaseSyntheticEvent,
+  useEffect,
+  useState,
+  SyntheticEvent
+} from 'react';
+import { Controller, useForm, useFieldArray } from 'react-hook-form';
+import { type RouterInputs, api } from '~/utils/api';
+import _ from 'lodash';
+import { Lembaga } from '~/utils/file';
+
+interface FormValues {
+  token: string;
+  location: string;
+}
+
+export const SecondForm = () => {
+  const { register, formState, setValue, getValues, reset } =
+    useForm<FormValues>({
+      mode: 'onChange',
+      delayError: 1000,
+      defaultValues: {
+        token: '',
+        location: ''
+      }
+    });
+
+  const [token, setToken] = useState<RouterInputs['showcase']['getLocation']>();
+  if (token) {
+    const data = api.showcase.getLocation.useQuery(token);
+  }
+  const registerLocation = api.showcase.bookLocation.useMutation();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const toast = useToast();
+
+  const fetchLocations = (e: SyntheticEvent) => {
+    // const test = (e.target as HTMLInputElement).value;
+    setToken({ token: (e.target as HTMLInputElement).value });
+    // void refetch();
+  };
+
+  // useEffect(() => {
+  //   console.log(data);
+  // }, [data]);
+
+  const submitFirstShowcase = async (
+    data: FormValues,
+    event: BaseSyntheticEvent
+  ) => {
+    event.preventDefault();
+    setLoading(true);
+
+    // try {
+    //   const result = await registerLocation.mutateAsync(data);
+
+    //   toast({
+    //     title: 'Success',
+    //     description: result.message,
+    //     status: 'success',
+    //     duration: 3000,
+    //     isClosable: true,
+    //     position: 'top'
+    //   });
+    //   reset();
+    // } catch (error: unknown) {
+    //   if (!(error instanceof TRPCClientError)) throw error;
+
+    //   toast({
+    //     title: 'Error',
+    //     description: error.message,
+    //     status: 'error',
+    //     duration: 3000,
+    //     isClosable: true,
+    //     position: 'top'
+    //   });
+    // }
+
+    setLoading(false);
+  };
+
+  return (
+    <Box
+      zIndex='10'
+      bgGradient='linear(to-br, navy.1, purple.3)'
+      boxShadow='inset 0 0 24px rgba(0,0,0,0.8), 12px 12px rgba(0,0,0,0.4)'
+      px={12}
+      py={9}
+      borderRadius='lg'
+      color='yellow.3'
+      maxH='70vh'
+      overflowY='auto'
+      w={{ base: '80%', lg: '700px' }}
+    >
+      <Heading
+        fontSize='2xl'
+        textAlign='center'
+        textShadow='4px 6px rgba(0,0,0,0.5)'
+      >
+        AMBIL LOKASI
+      </Heading>
+      <form onSubmit={(event) => void submitFirstShowcase(getValues(), event)}>
+        <VStack spacing={4} mt={5} color='white'>
+          <FormControl isRequired isInvalid={!!formState.errors.token}>
+            <FormLabel>Token</FormLabel>
+            <Input
+              placeholder='Masukkan token'
+              {...register('token', {
+                onChange: _.debounce(
+                  (e: SyntheticEvent) => fetchLocations(e),
+                  1000
+                )
+              })}
+            />
+            {formState.errors.token && (
+              <FormErrorMessage>
+                {formState.errors.token.message as string}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+          <FormControl isRequired isInvalid={!!formState.errors.location}>
+            <FormLabel>Lokasi</FormLabel>
+            <Select
+              variant='filled'
+              bg='gray.600'
+              color='white'
+              w='full'
+              borderColor='gray.400'
+              onChange={(e) =>
+                setValue(
+                  'location',
+                  Lembaga[e.target.value as keyof typeof Lembaga]
+                )
+              }
+              transition='all 0.2s ease-in-out'
+              _hover={{
+                opacity: 0.8
+              }}
+              _focus={{
+                background: 'gray.600',
+                borderColor: 'gray.400',
+                color: 'white'
+              }}
+              css={{
+                option: {
+                  background: '#2F2E2E'
+                }
+              }}
+            >
+              {Object.values(Lembaga).map((lembaga, index) => (
+                <option
+                  style={{
+                    background: 'gray.600',
+                    color: 'white'
+                  }}
+                  key={index}
+                  value={lembaga}
+                >
+                  {lembaga}
+                </option>
+              ))}
+            </Select>
+            {formState.errors.location && (
+              <FormErrorMessage>
+                {formState.errors.location.message as string}
+              </FormErrorMessage>
+            )}
+          </FormControl>
+        </VStack>
+        <Flex justifyContent='center' mt={7}>
+          <Button
+            alignSelf='center'
+            isLoading={loading}
+            loadingText='Mendaftarkan...'
+            type='submit'
+            isDisabled={Object.values(formState.errors).length > 0}
+          >
+            Daftar
+          </Button>
+        </Flex>
+      </form>
+    </Box>
+  );
+};
--- src/components/form/showcase/ShowCase.tsx
+++ src/components/form/showcase/ShowCase.tsx
@@ -1,51 +1,85 @@
-import { Flex, Image } from '@chakra-ui/react';
+import { Button, Flex, Image } from '@chakra-ui/react';
 import { FirstForm } from './FirstForm';
+import { SecondForm } from './SecondForm';
+import { useState } from 'react';
 
-export const ShowCase = () => (
-  <Flex
-    backgroundImage='/images/bg-coming-soon.png'
-    backgroundSize='cover'
-    backgroundPosition='center'
-    backgroundRepeat='no-repeat'
-    minH='100vh'
-    alignItems='center'
-    justifyContent='center'
-    position='relative'
-    px={5}
-  >
-    <Image
-      src='/images/spark.png'
-      alt=''
-      position='absolute'
-      w='30%'
-      top='0'
-      left='0'
-      zIndex='0'
-      draggable='false'
-      loading='lazy'
-    />
-    <Image
-      src='/images/spark3.png'
-      alt=''
-      position='absolute'
-      w='10%'
-      bottom='10%'
-      right='35%'
-      zIndex='0'
-      draggable='false'
-      loading='lazy'
-    />
-    <Image
-      src='/images/ornamen.png'
-      alt=''
-      position='absolute'
-      w='30%'
-      top='5%'
-      right='0'
-      zIndex='1'
-      draggable='false'
-      loading='lazy'
-    />
-    <FirstForm />
-  </Flex>
-);
+export const ShowCase = () => {
+  const [daftarUnit, setDaftarUnit] = useState(true);
+
+  return (
+    <Flex
+      backgroundImage='/images/bg-coming-soon.png'
+      backgroundSize='cover'
+      backgroundPosition='center'
+      backgroundRepeat='no-repeat'
+      minH='100vh'
+      alignItems='center'
+      justifyContent='center'
+      position='relative'
+      px={5}
+    >
+      <Image
+        src='/images/spark.png'
+        alt=''
+        position='absolute'
+        w='30%'
+        top='0'
+        left='0'
+        zIndex='0'
+        draggable='false'
+        loading='lazy'
+      />
+      <Image
+        src='/images/spark3.png'
+        alt=''
+        position='absolute'
+        w='10%'
+        bottom='10%'
+        right='35%'
+        zIndex='0'
+        draggable='false'
+        loading='lazy'
+      />
+      <Image
+        src='/images/ornamen.png'
+        alt=''
+        position='absolute'
+        w='30%'
+        top='5%'
+        right='0'
+        zIndex='1'
+        draggable='false'
+        loading='lazy'
+      />
+      <Flex direction={'column'} rowGap={'1rem'}>
+        <Flex
+          borderWidth={'3px'}
+          borderColor={'black'}
+          borderRadius={'50px'}
+          w={{ base: '80%', lg: '700px' }}
+          columnGap={'0.5rem'}
+        >
+          <Button
+            color={daftarUnit ? undefined : 'white'}
+            width={'50%'}
+            borderRadius={'50px'}
+            backgroundColor={daftarUnit ? '' : 'transparent'}
+            onClick={() => setDaftarUnit(true)}
+          >
+            Daftar Unit
+          </Button>
+          <Button
+            color={daftarUnit ? 'white' : undefined}
+            width={'50%'}
+            borderRadius={'50px'}
+            backgroundColor={daftarUnit ? 'transparent' : ''}
+            onClick={() => setDaftarUnit(false)}
+          >
+            Ambil Lokasi
+          </Button>
+        </Flex>
+        {daftarUnit ? <FirstForm /> : <SecondForm />}
+      </Flex>
+    </Flex>
+  );
+};
