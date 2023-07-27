2ff036985deb7f2eb3517b746a3456a40b20e2f4
Riswandha Mashuri <riswandhamash@gmail.com>
2023-07-28 02:16:14 +0700

    fix: create custom icon svg & add login menu for mobile and add dragable & loading for image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,9 +11,18 @@ import {
   MenuItem,
   MenuButton,
   MenuList,
+  Icon
 } from '@chakra-ui/react';
 import Link from 'next/link';
-import React, { ReactNode, useEffect, useState } from 'react';
+import React, { ReactNode, useState } from 'react';
+import {
+  AboutIcon,
+  MerchandiseIcon,
+  MapIcon,
+  BlogIcon,
+  LoginIcon,
+  SpacelogIcon
+} from '~/styles/component/icon';
 
 const MobLiItem = ({ children }: { children: ReactNode }) => {
   return (
@@ -66,8 +75,13 @@ const DeskLiItem = ({ children }: { children: ReactNode }) => {
 };
 
 const Navbar = () => {
-  const [isLogin, setIsLogin] = useState<Boolean>(false);
-  useEffect(() => {}, []);
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+
+  // contoh
+  const handleLogin = () => {
+    setIsLogin(!isLogin);
+  };
+
   return (
     <Center>
       <Flex
@@ -93,12 +107,14 @@ const Navbar = () => {
         fontWeight='semibold'
       >
         <Box pos='absolute' width='100%' left='0' top='0'>
-          <Image src='/images/nav-ekor.png' />
+          <Image src='/images/nav-ekor.png' draggable='false' loading='lazy' />
         </Box>
         <Box zIndex='1000'>
           <Image
             src='/images/nav-logo.svg'
             height={{ base: '38px', lg: '57px' }}
+            draggable='false'
+            loading='lazy'
           />
         </Box>
         <UnorderedList
@@ -118,7 +134,7 @@ const Navbar = () => {
             <Link href='/'>
               <DeskLiItem>Blog</DeskLiItem>
             </Link>
-            <Box>
+            <Box onClick={handleLogin}>
               {isLogin ? (
                 <Link href='/'>
                   <Button variant='solid'>Space Log</Button>
@@ -131,7 +147,7 @@ const Navbar = () => {
             </Box>
           </HStack>
         </UnorderedList>
-        <Box display={{ base: 'block', lg: 'none' }}>
+        <Box display={{ base: 'block', lg: '' }}>
           <Menu strategy='fixed'>
             <MenuButton
               as={Button}
@@ -140,7 +156,13 @@ const Navbar = () => {
               border='1px'
               borderRadius='8px'
             >
-              <Image src='/images/hamburger.png' width='24px' margin='auto' />
+              <Image
+                src='/images/hamburger.png'
+                width='24px'
+                margin='auto'
+                draggable='false'
+                loading='lazy'
+              />
             </MenuButton>
             <MenuList
               color='white'
@@ -154,32 +176,7 @@ const Navbar = () => {
               <Link href='/tes'>
                 <MobLiItem>
                   <Box>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path d='M6 9.5H4V16.5H6V9.5Z' fill='currentColor' />
-                      <path d='M12 9.5H10V16.5H12V9.5Z' fill='currentColor' />
-                      <path
-                        d='M21 5.5L11 0.5L1 5.5V7.5H21V5.5ZM5.47 5.5L11 2.74L16.53 5.5H5.47Z'
-                        fill='currentColor'
-                      />
-                      <path
-                        d='M1 18.5V20.5H13.4C13.19 19.86 13.08 19.19 13.04 18.5H1Z'
-                        fill='currentColor'
-                      />
-                      <path
-                        d='M18 11.76V9.5H16V12.76L18 11.76Z'
-                        fill='currentColor'
-                      />
-                      <path
-                        d='M19 13.5L15 15.5V18.05C15 20.57 16.71 22.93 19 23.5C21.29 22.93 23 20.57 23 18.05V15.5L19 13.5ZM18.28 20.5L16.25 18.47L17.31 17.41L18.28 18.38L20.69 16L21.75 17.06L18.28 20.5Z'
-                        fill='currentColor'
-                      />
-                    </svg>
+                    <Icon w={6} h={6} as={AboutIcon} />
                   </Box>
                   <Text>About Us</Text>
                 </MobLiItem>
@@ -187,18 +184,7 @@ const Navbar = () => {
               <Link href='/'>
                 <MobLiItem>
                   <Box>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M19 6.5H17C17 3.74 14.76 1.5 12 1.5C9.24 1.5 7 3.74 7 6.5H5C3.9 6.5 3 7.4 3 8.5V20.5C3 21.6 3.9 22.5 5 22.5H19C20.1 22.5 21 21.6 21 20.5V8.5C21 7.4 20.1 6.5 19 6.5ZM12 3.5C13.66 3.5 15 4.84 15 6.5H9C9 4.84 10.34 3.5 12 3.5ZM19 20.5H5V8.5H19V20.5ZM12 12.5C10.34 12.5 9 11.16 9 9.5H7C7 12.26 9.24 14.5 12 14.5C14.76 14.5 17 12.26 17 9.5H15C15 11.16 13.66 12.5 12 12.5Z'
-                        fill='currentColor'
-                      />
-                    </svg>
+                    <Icon w={6} h={6} as={MerchandiseIcon} />
                   </Box>
                   <Text>Merchandise</Text>
                 </MobLiItem>
@@ -206,18 +192,7 @@ const Navbar = () => {
               <Link href='/'>
                 <MobLiItem>
                   <Box>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M20.5 3L20.34 3.03L15 5.1L9 3L3.36 4.9C3.15 4.97 3 5.15 3 5.38V20.5C3 20.78 3.22 21 3.5 21L3.66 20.97L9 18.9L15 21L20.64 19.1C20.85 19.03 21 18.85 21 18.62V3.5C21 3.22 20.78 3 20.5 3ZM10 5.47L14 6.87V18.53L10 17.13V5.47ZM5 6.46L8 5.45V17.15L5 18.31V6.46ZM19 17.54L16 18.55V6.86L19 5.7V17.54Z'
-                        fill='currentColor'
-                      />
-                    </svg>
+                    <Icon w={6} h={6} as={MapIcon} />
                   </Box>
                   <Text>Interactive Map</Text>
                 </MobLiItem>
@@ -225,22 +200,32 @@ const Navbar = () => {
               <Link href='/'>
                 <MobLiItem>
                   <Box>
-                    <svg
-                      width='24'
-                      height='24'
-                      viewBox='0 0 24 24'
-                      fill='none'
-                      xmlns='http://www.w3.org/2000/svg'
-                    >
-                      <path
-                        d='M4 6H2V20C2 21.1 2.9 22 4 22H18V20H4V6ZM20 2H8C6.9 2 6 2.9 6 4V16C6 17.1 6.9 18 8 18H20C21.1 18 22 17.1 22 16V4C22 2.9 21.1 2 20 2ZM20 16H8V4H20V16ZM10 9H18V11H10V9ZM10 12H14V14H10V12ZM10 6H18V8H10V6Z'
-                        fill='currentColor'
-                      />
-                    </svg>
+                    <Icon w={6} h={6} as={BlogIcon} />
                   </Box>
                   <Text>Blog</Text>
                 </MobLiItem>
               </Link>
+              <Box onClick={handleLogin}>
+                {isLogin ? (
+                  <Link href='/'>
+                    <MobLiItem>
+                      <Box>
+                        <Icon w={6} h={6} as={SpacelogIcon} />
+                      </Box>
+                      <Text>Spacelog</Text>
+                    </MobLiItem>
+                  </Link>
+                ) : (
+                  <Link href='/'>
+                    <MobLiItem>
+                      <Box>
+                        <Icon w={6} h={6} as={LoginIcon} />
+                      </Box>
+                      <Text>Login</Text>
+                    </MobLiItem>
+                  </Link>
+                )}
+              </Box>
             </MenuList>
           </Menu>
         </Box>
