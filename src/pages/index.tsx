66f42e361b7272c8ab65d84846384f5e4d751302
msyahrulsp <mssp892@gmail.com>
2023-08-11 08:10:14 +0700

    chore: merge staging

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,39 @@
 import { type NextPage } from 'next';
+import { Flex } from '@chakra-ui/react';
 import Layout from '~/layout';
-import Timeline from '~/components/Timeline';
+import ImageBox from '~/components/ImageBox';
+import Jumbotron from '~/components/home-page/Jumbotron';
 
 const Home: NextPage = () => {
   return (
-    <Layout title={''}>
-      {/* jumbotron */}
-      {/* timleine */}
-      {/* sponsor */}
-      {/* medpar */}
-      <Timeline></Timeline>
+    <Layout>
+      <Flex
+        minH='100dvh'
+        width='100%'
+        backgroundImage='/images/bg-landing.png'
+        backgroundSize='cover'
+        backgroundPosition='center'
+        backgroundRepeat='no-repeat'
+        alignItems='center'
+        justifyContent='center'
+        flexDirection='column'
+        gap={5}
+      >
+        <Jumbotron />
+        {/* timleine */}
+        <ImageBox
+          title='OUR SPONSORS'
+          object='SPONSORS'
+          contact='loremipsum@gmail.com'
+          image='/images/bg-sponsor.png'
+        />
+        <ImageBox
+          title='OUR MEDIA PARTNER'
+          object='MEDIA&nbsp;PARTNER'
+          contact='+62-812-9237-2312'
+          image='/images/bg-media-partner.png'
+        />
+      </Flex>
     </Layout>
   );
 };
