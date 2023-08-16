63842bf95394fa73b081ae4a88455ca617f8478d
msyahrulsp <mssp892@gmail.com>
2023-08-16 20:32:47 +0700

    feat: disable interactive map query

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -1,4 +1,4 @@
-import { type Dispatch, useState, type SetStateAction, useEffect } from 'react';
+import { type Dispatch, useState, type SetStateAction, useEffect } from "react";
 import {
   Button,
   Image,
@@ -16,8 +16,8 @@ import {
   Heading,
   HStack,
   Box
-} from '@chakra-ui/react';
-import mapboxgl from 'mapbox-gl';
+} from "@chakra-ui/react";
+import mapboxgl from "mapbox-gl";
 import {
   Map,
   type ViewState,
@@ -27,14 +27,14 @@ import {
   FullscreenControl,
   useMap,
   Marker
-} from 'react-map-gl';
-import { type MapLocation, type MapPhoto } from '@prisma/client';
-import { api } from '~/utils/api';
-import { env } from '~/env.cjs';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import Slider from 'react-slick';
-import { Dots } from './about-us/Dots';
-import MapModalBackground from './background/MapModalBackground';
+} from "react-map-gl";
+import { type MapLocation, type MapPhoto } from "@prisma/client";
+import { api } from "~/utils/api";
+import { env } from "~/env.cjs";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Slider from "react-slick";
+import { Dots } from "./about-us/Dots";
+import MapModalBackground from "./background/MapModalBackground";
 
 const InteractiveSelect = ({
   placeholder,
@@ -90,7 +90,7 @@ const InteractiveSelect = ({
       }}
       css={{
         option: {
-          background: '#2B0792'
+          background: "#2B0792"
         }
       }}
     >
@@ -159,7 +159,7 @@ const InteractiveMarker = ({
         draggable='false'
         loading='lazy'
         _hover={{
-          cursor: 'pointer',
+          cursor: "pointer",
           animation: `${scaling} 0.5s ease-in-out infinite alternate`
         }}
       />
@@ -227,7 +227,7 @@ const InteractiveMap = ({
   });
 
   const [selectedCampus, setSelectedCampus] = useState<string>(
-    inputSelectedCampus ?? 'Ganesha'
+    inputSelectedCampus ?? "Ganesha"
   );
   const [selectedLocation, setSelectedLocation] = useState<
     | (MapLocation & {
@@ -243,9 +243,9 @@ const InteractiveMap = ({
   const { isOpen, onOpen, onClose } = useDisclosure();
   const toast = useToast();
 
-  const getLocationsQuery = api.interactiveMap.getLocations.useQuery({
-    campus: selectedCampus
-  });
+  // const getLocationsQuery = api.interactiveMap.getLocations.useQuery({
+  //   campus: selectedCampus
+  // });
 
   const settings = {
     dots: false,
@@ -272,12 +272,12 @@ const InteractiveMap = ({
       )
       .then(() =>
         toast({
-          title: 'Success',
-          description: 'Copied to clipboard!',
-          status: 'success',
+          title: "Success",
+          description: "Copied to clipboard!",
+          status: "success",
           duration: 3000,
           isClosable: true,
-          position: 'top'
+          position: "top"
         })
       );
   };
@@ -288,26 +288,26 @@ const InteractiveMap = ({
       mapLib={mapboxgl}
       mapStyle='mapbox://styles/mapbox/streets-v12'
       style={{
-        width: '100%',
-        height: '100vh'
+        width: "100%",
+        height: "100vh"
       }}
       onMove={(e) => setViewState(e.viewState)}
       {...viewState}
     >
       <FullscreenControl
         style={{
-          position: 'relative',
-          top: '100px',
-          right: '20px'
+          position: "relative",
+          top: "100px",
+          right: "20px"
         }}
       />
       <NavigationControl
-        style={{ position: 'relative', top: '100px', right: '20px' }}
+        style={{ position: "relative", top: "100px", right: "20px" }}
       />
       <GeolocateControl
         showUserHeading
         trackUserLocation
-        style={{ position: 'relative', top: '100px', right: '20px' }}
+        style={{ position: "relative", top: "100px", right: "20px" }}
       />
       <ScaleControl />
       <InteractiveSelect
@@ -322,22 +322,22 @@ const InteractiveMap = ({
           onOpen={onOpen}
         />
       )}
-      {getLocationsQuery.data?.map((location) => (
+      {/* {getLocationsQuery.data?.map((location) => (
         <InteractiveMarker
           key={location.id}
           location={location}
           setSelectedLocation={setSelectedLocation}
           onOpen={onOpen}
         />
-      ))}
+      ))} */}
       <Modal isOpen={isOpen} onClose={onClose} size='2xl'>
         <ModalOverlay />
         <ModalContent
           bg='navy.1'
           containerProps={{
-            alignItems: 'center',
-            justifyContent: isLg ? 'flex-end' : 'center',
-            px: isLg ? '2rem' : '1rem'
+            alignItems: "center",
+            justifyContent: isLg ? "flex-end" : "center",
+            px: isLg ? "2rem" : "1rem"
           }}
           maxH='80vh'
         >
@@ -349,16 +349,16 @@ const InteractiveMap = ({
             pb={6}
             overflowY='scroll'
             sx={{
-              '&::-webkit-scrollbar': {
-                borderRadius: '144px',
-                bg: 'transparent'
+              "&::-webkit-scrollbar": {
+                borderRadius: "144px",
+                bg: "transparent"
               },
-              '&::-webkit-scrollbar-track': {
-                borderRadius: '144px',
-                bg: 'transparent'
+              "&::-webkit-scrollbar-track": {
+                borderRadius: "144px",
+                bg: "transparent"
               },
-              '&::-webkit-scrollbar-thumb': {
-                borderRadius: '144px'
+              "&::-webkit-scrollbar-thumb": {
+                borderRadius: "144px"
               }
             }}
           >
@@ -397,7 +397,7 @@ const InteractiveMap = ({
                             borderRadius='lg'
                             p={3}
                             bg='yellow.5'
-                            w={{ base: '200px', lg: '50%' }}
+                            w={{ base: "200px", lg: "50%" }}
                           >
                             <Image
                               src={photo.imageUrl}
@@ -424,7 +424,7 @@ const InteractiveMap = ({
                       borderRadius='lg'
                       p={3}
                       bg='yellow.5'
-                      w={{ base: '200px', lg: '250px' }}
+                      w={{ base: "200px", lg: "250px" }}
                     >
                       <Image
                         src={selectedLocation.MapPhoto[0]?.imageUrl}
@@ -445,7 +445,7 @@ const InteractiveMap = ({
                     borderRadius='lg'
                     p={3}
                     bg='yellow.5'
-                    w={{ base: '200px', lg: '250px' }}
+                    w={{ base: "200px", lg: "250px" }}
                   >
                     <Image
                       src='/images/misc/spark3.png'
